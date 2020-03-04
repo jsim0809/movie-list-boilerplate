964f906386e1418eb01a18a21229c7c100728a0a
Hack Reactor Student <student@hackreactor.com>
2020-03-03 21:02:21 -0800

    Most basic rendering works

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import MovieList from './MovieList';
 
-const App = (props) => (
-  <div>Hello World!</div>
+
+const App = ({movies}) => (
+  <div>
+    <div>MovieList</div>
+    <MovieList movies={movies}/>
+  </div>
 );
 
 export default App;
